71f7be34dc3f73171d841fbec5def238339dbb81
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2020-11-08 22:34:50 +0700

    Method reverse

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,24 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	int n = sc.nextInt();
-	int ans = 0, digit, x=n;
-	while(n!=0){
-	    digit = n%10;
-	    ans = ans*10+digit;
-	    n = n/10;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("reverse of " + n + " is " + reverse(n));
+
+
     }
-        System.out.println(ans);
+
+    public static int reverse(int n) {
+        int ans = 0, digit, x = n;
+        while (n != 0) {
+            digit = n % 10;
+            ans = ans * 10 + digit;
+            n = n / 10;
+        }
+        return ans;
     }
 }
