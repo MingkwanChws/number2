8c5ddac8d03b89dbc77848054d5cf21106ad36f0
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2020-11-08 22:30:23 +0700

    Add reverse number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int ans = 0, digit, x=n;
+	while(n!=0){
+	    digit = n%10;
+	    ans = ans*10+digit;
+	    n = n/10;
+    }
+        System.out.println(ans);
     }
 }
